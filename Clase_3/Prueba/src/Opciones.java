c50632271370ed31cac64b02ca8f2ddb54899b17
Javier <birmania221524@gmail.co>
2026-06-26 08:02:11 -0500

    repaso clase 3 java

---
+++ Clase_3/Prueba/src/Opciones.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Opciones {
+
+  public static void main(String[] args) {
+
+    Scanner entrada = new Scanner(System.in);
+
+    System.out.println(" Ingrese el numero de pedido: ");
+    int opcion = entrada.nextInt();
+
+    switch (opcion) {
+      case 1:
+        System.out.println("nuevo padido");
+        break;
+      case 2:
+        System.out.println("Aplicar descuento");
+        break;
+      case 3:
+        System.out.println("Cerrar caja");
+        break;
+
+      default:
+        System.out.println("Opcion Invalida.....");
+        break;
+    }
+  }
+
+}
